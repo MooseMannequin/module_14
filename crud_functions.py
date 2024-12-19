34559c17f491a978346aaedff14fce9804377506
MooseMannequin <EugeneNeudene@yandex.ru>
2024-12-19 20:12:49 +0300

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -13,9 +13,40 @@ def initiate_db():
     )
     ''')
 
-    for i in range(1,5):
-        cursor.execute(' INSERT INTO Products (title, description, price) VALUES (?, ?, ?)',
-                       (f'Продукт{i}', f'Описание{i}', f'{i*100}'))
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+    )
+    ''')
+
+    connection.commit()
+    connection.close()
+
+def add_user(username, email, age):
+    connection = sqlite3.connect('Products.db')
+    cursor = connection.cursor()
+
+
+    cursor.execute(f"INSERT INTO Users (username, email, age, balance)"
+                   f" VALUES ('{username}', '{email}', '{age}', 1000)")
+
+    connection.commit()
+    connection.close()
+
+def is_included(username):
+    connection = sqlite3.connect('Products.db')
+    cursor = connection.cursor()
+
+    check_user = cursor.execute(' SELECT * FROM Users WHERE username = ?', (username,))
+
+    if check_user.fetchone() is None:
+        return False
+    else:
+        return True
 
     connection.commit()
     connection.close()
@@ -26,6 +57,20 @@ def get_all_products():
 
     cursor.execute(' SELECT title, description, price FROM Products')
     all_products = cursor.fetchall()
+    connection.close()
     return all_products
 
-get_all_products()
+
+
+
+# initiate_db()
+#
+# connection = sqlite3.connect('Products.db')
+# cursor = connection.cursor()
+#
+# for i in range(1, 5):
+#     cursor.execute(' INSERT INTO Products (title, description, price) VALUES (?, ?, ?)',
+#                    (f'Продукт{i}', f'Описание{i}', f'{i * 100}'))
+#
+# connection.commit()
+# connection.close()
